5657c067e6d786538c36ff1a15b50732086557ef
Rui Afonso Rodrigues <ruiafonsor@outlook.com>
2024-08-08 00:22:00 +0100

    App successfully syncs 2 folders

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            var source = @"C:\projects\SyncTwoFolders\folders\source";
+            var replica = @"C:\projects\SyncTwoFolders\folders\replica";
+
+            var syncCommand = new SyncCommand(source, replica);
+            var fileManager = new FileManager(syncCommand);
+
+            fileManager.ExecuteCommand();
+
+            Console.ReadKey();
         }
     }
 }
--- SyncCommand.cs
+++ SyncCommand.cs
@@ -26,30 +26,61 @@
             var destinationFileList = Directory.GetFiles(destinationPath);
             var destinationDirectoryList = Directory.GetDirectories(destinationPath);
 
-            // copies all files from source to replica
-            foreach (var file in sourceFileList)
+            try
             {
-                File.Copy(file, _destination, true);
+                // copies all files from source to replica
+                foreach (var file in sourceFileList)
+                {
+                    File.Copy(file, file.Replace(sourcePath, destinationPath), true);
+                }
+
+                // copies all directories from source to replica
+                foreach (var directory in sourceDirectoryList)
+                {
+                    if (!Directory.Exists(directory.Replace(sourcePath, destinationPath)))
+                    {
+                        CopyDirectory(sourcePath, destinationPath);
+                    }
+                }
+
+                // deletes all files from replica that do not exist on source anymore
+                foreach (var file in destinationFileList)
+                {
+                    if (!File.Exists(sourcePath + Path.GetFileName(file)))
+                        File.Delete(file);
+                }
+
+                // deletes all directories from replica that do not exist on source anymore
+                foreach (var directory in destinationDirectoryList)
+                {
+                    if (!Directory.Exists(sourcePath + Path.GetDirectoryName(directory)))
+                        Directory.Delete(directory);
+                }
+
+                Console.WriteLine("Sync Completed!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void CopyDirectory(string sourceDirectory, string destinationDirectory)
+        {
+            Directory.CreateDirectory(destinationDirectory);
+
+            // Copy all the files
+            foreach (string file in Directory.GetFiles(sourceDirectory))
+            {
+                string destFile = Path.Combine(destinationDirectory, Path.GetFileName(file));
+                File.Copy(file, destFile, true);
             }
 
-            // copies all directories from source to replica
-            foreach (var directory in sourceDirectoryList)
+            // Copy all the subdirectories
+            foreach (string subdir in Directory.GetDirectories(sourceDirectory))
             {
-                File.Copy(directory, _destination, true);
-            }
-
-            // deletes all files from replica that do not exist on source anymore
-            foreach(var file in destinationFileList)
-            {
-                if (!File.Exists(sourcePath + Path.GetFileName(file)))
-                    File.Delete(file);
-            }
-
-            // deletes all directories from replica that do not exist on source anymore
-            foreach (var file in destinationDirectoryList)
-            {
-                if (!Directory.Exists(sourcePath + Path.GetFileName(file)))
-                    File.Delete(file);
+                string destSubdir = Path.Combine(destinationDirectory, Path.GetFileName(subdir));
+                CopyDirectory(subdir, destSubdir);
             }
         }
     }
